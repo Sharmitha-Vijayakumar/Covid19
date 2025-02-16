2094eab01b6833399e6c19f2aa7f6c077e39e428
Sharmitha-Vijayakumar <sharmithavijayakumarofficial@gmail.com>
2025-02-16 22:46:40 +0530

    Delete industry.py

--- industry.py
+++
@@ -1,8 +0,0 @@
-sectors = ['Agriculture', 'Aviation', 'Education', 'Retail', 'Tourism']
-impact_scores = [10, 20, 15, 12, 25]  # Example scores
-
-plt.figure(figsize=(8, 8))
-plt.pie(impact_scores, labels=sectors, autopct='%1.1f%%', startangle=90)
-plt.title('Economic Impact of COVID-19 by Sector')
-plt.tight_layout()
-plt.show()
